523b4f396bfa1e65c47725c7555f7e4a602c1d78
Mike King <mike@85pixels.com>
2013-10-11 00:50:39 -1000

    Updated Gruntfile.js; removed bower_components from copy function

--- Gruntfile.js
+++ Gruntfile.js
@@ -240,7 +240,6 @@ module.exports = function (grunt) {
 					src: [
 						'*.{ico,png,txt}',
 						'.htaccess',
-						'bower_components/**/*',
 						'images/{,*/}*.{gif,webp}',
 						'fonts/**/*',
 						'CNAME'
